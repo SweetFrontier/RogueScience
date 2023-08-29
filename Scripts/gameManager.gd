7890a6043eda29769a456609109910059a58e941
Ninjajkl <ninjajkl@gmail.com>
2023-08-29 00:04:13 -0400

    feat: added minimum viable scene

---
+++ Scripts/gameManager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# Use @export to make the array editable in the inspector
+@export var Levels: Array[levelController]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ Scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# Store the current movement direction.
+var current_direction = Vector2.RIGHT
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Check if there's a wall in the current direction.
+	if is_on_wall():
+		current_direction = -current_direction
+
+	# Move in the current direction.
+	velocity.x = current_direction.x * SPEED
+
+	move_and_slide()

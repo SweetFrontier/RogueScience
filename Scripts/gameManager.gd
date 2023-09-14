125b3a2e1aba6b1d534321d0fe2253d155a8b3f3
CaptainIndia <40968890+Sakarmit@users.noreply.github.com>
2023-09-14 19:44:04 -0400

    Start Level after moving and zoom

--- Scripts/gameManager.gd
+++ Scripts/gameManager.gd
@@ -16,26 +16,29 @@ func _ready() -> void:
 
 func _process(delta):
 	if (moving):
-		# If you move to a new level start that level
-		Levels[current_level-1].process_mode = Node.PROCESS_MODE_DISABLED
-		Levels[current_level].process_mode = Node.PROCESS_MODE_ALWAYS
 		# Smooth movement towards new level center
 		var final_pos : Vector2 = Levels[current_level].cameraSpot.global_position
 		var smooth_move_x = lerp(camera.position.x, final_pos.x, 3 * delta)
 		var smooth_move_y = lerp(camera.position.y, final_pos.y, 3 * delta)
-		if (smooth_move_x != final_pos.x or smooth_move_y != final_pos.y):
+		if abs(smooth_move_x - final_pos.x) > 10 or abs(smooth_move_y - final_pos.y) > 1:
 			camera.position = Vector2(smooth_move_x, smooth_move_y)
 		else:
 			moving = false
 		
 	if (zooming):
 		var final_zoom : Vector2 =  Levels[current_level].cameraSize
-		var smooth_zoom_x = lerp(camera.zoom.x, 1280/final_zoom.x, 3 * delta)
-		var smooth_zoom_y = lerp(camera.zoom.y, 736/final_zoom.y, 3 * delta)
-		if smooth_zoom_x != final_zoom.x or smooth_zoom_y != final_zoom.y:
+		final_zoom.x = 1280/final_zoom.x
+		final_zoom.y = 736/final_zoom.y
+		var smooth_zoom_x = lerp(camera.zoom.x, final_zoom.x, 3 * delta)
+		var smooth_zoom_y = lerp(camera.zoom.y, final_zoom.y, 3 * delta)
+		if abs(smooth_zoom_x - final_zoom.x) > 0.1 or abs(smooth_zoom_y - final_zoom.y) > 0.1:
 			camera.set_zoom(Vector2(smooth_zoom_x, smooth_zoom_y))
 		else:
 			zooming = false
+
+	if (not(moving or zooming)):
+		Levels[current_level-1].process_mode = Node.PROCESS_MODE_DISABLED
+		Levels[current_level].process_mode = Node.PROCESS_MODE_ALWAYS
 
 func increase_level() -> void:
 	current_level += 1

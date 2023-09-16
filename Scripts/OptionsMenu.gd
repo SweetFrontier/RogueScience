e0995b86dacea34d6c73701f7735c73a04597d98
JusPSLiu <juspsliu@yahoo.com>
2023-09-16 12:40:33 -0400

    fixed sound slider & made game quieter

--- Scripts/OptionsMenu.gd
+++ Scripts/OptionsMenu.gd
@@ -1,9 +1,14 @@
 extends Control
 
-signal close_settings
 
-@export var volume = 10
+signal close_settings
+#settingUp variable so when loading, doesn't play sounds
+var settingUp : bool = true
+var timer : float = 0
+
+@export var sound_slider_stop_time := 0.5
 @export var soundSlider : HSlider
+@export var soundPlayer : AudioStreamPlayer
 @export var musicSlider : HSlider
 
 func _ready():
@@ -21,6 +26,12 @@ func _on_h_slider_value_changed(value: float) -> void:
 
 func _on_h_slider_2_value_changed(value: float):
 	AudioServer.set_bus_volume_db(1, linear_to_db(value))
+	if (settingUp):
+		settingUp = false
+	else:
+		timer = sound_slider_stop_time
+		soundPlayer.volume_db = 2;
+		soundPlayer.play()
 
 func _on_pause_menu_pressed() -> void:
 	if (get_tree().current_scene.name == "Main"):
@@ -29,3 +40,11 @@ func _on_pause_menu_pressed() -> void:
 		emit_signal("close_settings")
 	visible = false
 
+func _process(delta):
+	if (timer > 0):
+		timer -= delta;
+	elif (soundPlayer.playing):
+		if (soundPlayer.volume_db > -60):
+			soundPlayer.volume_db -= delta*100;
+		else:
+			soundPlayer.stop()

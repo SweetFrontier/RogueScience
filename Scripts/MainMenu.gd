a56dd4e689fbc35364c4be8d237b7dd8f9c41e2f
JusPSLiu <juspsliu@yahoo.com>
2023-09-22 17:36:52 -0400

    loading from title to levels

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -3,6 +3,7 @@ extends Control
 @export var soundSettings : Control
 @export var titleScreen : Control
 @export var buttonSound : AudioStreamPlayer
+@export var transition : AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +14,10 @@ func close_settings() -> void:
 	buttonSound.play()
 
 func _on_start_button_pressed():
+	transition.show()
+	transition.play("CoverScreen")
+	buttonSound.play()
+	await(transition.animation_finished)
 	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
 
 func _on_credits_button_pressed():

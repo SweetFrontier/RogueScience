be54500174f4552a82a43e3953c8e1f00a6b98e4
JusPSLiu <juspsliu@yahoo.com>
2023-09-24 01:36:54 -0400

    added title and revised splashscreen

--- Scripts/splashscreen.gd
+++ Scripts/splashscreen.gd
@@ -1,6 +1,8 @@
 extends Control
 
 @export var splashscreenVideo : VideoStreamPlayer
+@export var transitionToBlack : AnimationPlayer
+@export var buttonSound : AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +11,10 @@ func _ready():
 func _on_video_stream_player_finished():
 	get_tree().change_scene_to_file("res://Scenes/Screens/MainMenu.tscn")
 	pass # Replace with function body.
+
+func _input(event : InputEvent):
+	if (event is InputEventKey and event.keycode == KEY_ESCAPE) or (event is InputEventMouseButton and event.is_double_click()):
+			transitionToBlack.play("FadeToBlack")
+			buttonSound.play()
+			await transitionToBlack.animation_finished
+			get_tree().change_scene_to_file("res://Scenes/Screens/MainMenu.tscn")

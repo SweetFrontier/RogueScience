71c284fb74888658b3b8fb2c755ce64de0d2ee55
JusPSLiu <juspsliu@yahoo.com>
2023-09-26 02:48:24 -0400

    Added splashscreen skip instructions

--- Scripts/godotSplashscreen.gd
+++ Scripts/godotSplashscreen.gd
@@ -2,11 +2,23 @@ extends Control
 
 @export var transitionToBlack : AnimationPlayer
 @export var buttonSound : AudioStreamPlayer
+@export var SkipButton : AnimationPlayer
+@export var howLongSkipButtonVisible : float = 1.2
+
+var skipTimer : float = 0;
+var skipVisible : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _process(delta):
+	if (skipTimer > 0):
+		skipTimer -= delta
+		if (skipTimer <= 0):
+			skipTimer = 0
+			SkipButton.play("skipDisappear")
+			skipVisible = false
 
 func _on_animation_player_animation_finished(anim_name):
 	get_tree().change_scene_to_file("res://Scenes/Other/splashscreen.tscn")
@@ -17,4 +29,11 @@ func _input(event : InputEvent):
 			buttonSound.play()
 			await transitionToBlack.animation_finished
 			get_tree().change_scene_to_file("res://Scenes/Other/splashscreen.tscn")
+	elif (event is InputEventMouseMotion):
+		#play animation only if not already visible
+		if (!skipVisible):
+			SkipButton.play("skipAppear")
+		#set timer for skip to disappear for howLongSkipButtonVisible seconds
+		skipTimer = howLongSkipButtonVisible
+		skipVisible = true
 
--- Scripts/splashscreen.gd
+++ Scripts/splashscreen.gd
@@ -3,10 +3,23 @@ extends Control
 @export var splashscreenVideo : VideoStreamPlayer
 @export var transitionToBlack : AnimationPlayer
 @export var buttonSound : AudioStreamPlayer
+@export var SkipButton : AnimationPlayer
+@export var howLongSkipButtonVisible : float = 1.2
+
+var skipTimer : float = 0;
+var skipVisible : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func _process(delta):
+	if (skipTimer > 0):
+		skipTimer -= delta
+		if (skipTimer <= 0):
+			skipTimer = 0
+			SkipButton.play("skipDisappear")
+			skipVisible = false
 
 func _on_video_stream_player_finished():
 	get_tree().change_scene_to_file("res://Scenes/Screens/MainMenu.tscn")
@@ -18,3 +31,10 @@ func _input(event : InputEvent):
 			buttonSound.play()
 			await transitionToBlack.animation_finished
 			get_tree().change_scene_to_file("res://Scenes/Screens/MainMenu.tscn")
+	elif (event is InputEventMouseMotion):
+		#play animation only if not already visible
+		if (!skipVisible):
+			SkipButton.play("skipAppear")
+		#set timer for skip to disappear for howLongSkipButtonVisible seconds
+		skipTimer = howLongSkipButtonVisible
+		skipVisible = true

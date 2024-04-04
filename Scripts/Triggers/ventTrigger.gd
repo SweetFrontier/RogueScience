81ca388f60db02fcdfd955b36c34798cee8b2e8f
Joey Parker <ninjajkl@gmail.com>
2024-04-04 00:39:23 -0400

    feat: finished vent and made 2 levels

--- Scripts/Triggers/ventTrigger.gd
+++ Scripts/Triggers/ventTrigger.gd
@@ -2,6 +2,7 @@ extends baseTrigger
 
 class_name ventTrigger
 
+@export var ventAnimSprites: Array[AnimatedSprite2D]
 @export var ventExtranceDirection: Direction
 @export var ventExitDirection: Direction
 @export var entrance: Node2D
@@ -12,6 +13,7 @@ class_name ventTrigger
 
 var rider_freeable = false
 var currState: VentState = VentState.CLOSED
+var currVentIndex = 0
 
 enum VentState
 {
@@ -47,6 +49,9 @@ func _ready():
 		exit.scale.x = 1
 	else:
 		exit.scale.x = -1
+	for child in $VentPath.get_children():
+		ventAnimSprites.append(child as AnimatedSprite2D)
+	determineVentOrientations()
 	reset()
 
 func react():
@@ -62,6 +67,15 @@ func reset():
 	currState = VentState.CLOSED
 	entranceSprite.animation = stateToAnimString[currState]
 	doorStop.visible = false
+	currVentIndex = 0
+	for vent in ventAnimSprites:
+		vent.frame = 0
+		vent.stop()
+	if startActivated:
+		react()
+		if show_button:
+			button_fade_timer = 0
+			TriggerKeySprite.modulate.a = 0
 
 func _physics_process(delta):
 	super._physics_process(delta)
@@ -81,6 +95,7 @@ func resetVenting():
 	currState = VentState.OPENING
 	entranceSprite.animation = stateToAnimString[currState]
 	entranceSprite.play()
+	currVentIndex = 0
 
 func onShutVentAreaEntered(body):
 	if !body is rigidPlayer and !body is movingObject:
@@ -96,16 +111,115 @@ func onInsideCheckVentAreaEntered(body):
 	if !body is rigidPlayer and !body is movingObject:
 		return
 	if currState == VentState.READYTOVENT and !occupied and body != self:
+		if body.movement_overrider != null:
+			body.movement_overrider.free_movement()
 		override_movement(body)
 		#Uncomment to keep momentum lol
-		#body.set_freed_vel(body.angular_velocity, body.linear_velocity)
+		#if body is movingObject:
+			#body.set_freed_vel(body.angular_velocity, body.linear_velocity)
 		rider_freeable = false
 		ridingBody.hide()
 		riderReady()
-		teleportRider()
-		resetVenting()
+		ventAnimSprites[0].play()
 
 func onEntranceAnimationFinished():
 	if entranceSprite.animation == stateToAnimString[VentState.OPENING]:
 		currState = VentState.OPEN
 		entranceSprite.animation = stateToAnimString[currState]
+
+func onVentFrameChanged():
+	if currVentIndex >= ventAnimSprites.size():
+		return
+	var currVent = ventAnimSprites[currVentIndex]
+	if currVent.frame == 8:
+		currVentIndex += 1
+		if currVentIndex >= ventAnimSprites.size():
+			teleportRider()
+			resetVenting()
+			return
+		currVent = ventAnimSprites[currVentIndex]
+		currVent.play()
+
+func determineVentOrientations():
+	var orientations = []
+	for i in range(ventAnimSprites.size()):
+		var current = ventAnimSprites[i]
+		var prev = ventAnimSprites[i - 1] if i > 0 else entrance
+		
+		var x_diff = current.global_position.x - prev.global_position.x
+		var y_diff = current.global_position.y - prev.global_position.y
+		
+		if x_diff == 0:
+			if y_diff == 0:
+				#Don't do anything, 'cause broke
+				print_debug("Vent on top of adjacent vent")
+			elif y_diff > 0:
+				if i > 0:
+					orientations[i-1] += 1 << 6
+				orientations.append(1 << 3)
+			else:
+				if i > 0:
+					orientations[i-1] += 1 << 7
+				orientations.append(1 << 2)
+		elif x_diff > 0:
+			if i > 0:
+				orientations[i-1] += 1 << 4
+			orientations.append(1 << 1)
+		else:
+			if i > 0:
+				orientations[i-1] += 1 << 5
+			orientations.append(1 << 0)
+	orientations[orientations.size()-1] += 1<<5 if ventExitDirection == Direction.LEFT else 1<<4
+	
+	for i in range(orientations.size()):
+		match orientations[i]:
+			132:
+				#"Down - Up"
+				ventAnimSprites[i].animation = "straight"
+			130:
+				#"Left - Up"
+				ventAnimSprites[i].animation = "curved"
+				ventAnimSprites[i].rotate(deg_to_rad(90))
+			129:
+				#"Right - Up"
+				ventAnimSprites[i].animation = "curved"
+				ventAnimSprites[i].rotate(deg_to_rad(-90))
+				ventAnimSprites[i].flip_h = true
+			72:
+				#"Up - Down"
+				ventAnimSprites[i].animation = "straight"
+				ventAnimSprites[i].flip_v = true
+			66:
+				#"Left - Down"
+				ventAnimSprites[i].animation = "curved"
+				ventAnimSprites[i].rotate(deg_to_rad(90))
+				ventAnimSprites[i].flip_h = true
+			65:
+				#"Right - Down"
+				ventAnimSprites[i].animation = "curved"
+				ventAnimSprites[i].rotate(deg_to_rad(-90))
+			40:
+				#"Up - Left"
+				ventAnimSprites[i].animation = "curved"
+				ventAnimSprites[i].flip_v = true
+			36:
+				#"Down - Left"
+				ventAnimSprites[i].animation = "curved"
+			33:
+				#"Right - Left"
+				ventAnimSprites[i].animation = "straight"
+				ventAnimSprites[i].rotate(deg_to_rad(-90))
+			24:
+				#"Up - Right"
+				ventAnimSprites[i].animation = "curved"
+				ventAnimSprites[i].flip_v = true
+				ventAnimSprites[i].flip_h = true
+			20:
+				#"Down - Right"
+				ventAnimSprites[i].animation = "curved"
+				ventAnimSprites[i].flip_h = true
+			18:
+				#"Left - Right"
+				ventAnimSprites[i].animation = "straight"
+				ventAnimSprites[i].rotate(deg_to_rad(90))
+	
